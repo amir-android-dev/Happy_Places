2fb64af94593556f37f63a53b4bbd831f715f77d
amir-android-dev <amir-hosseini@gmx.at>
2022-03-29 23:33:03 +0200

    12 Reading the data from our local DB

--- app/src/main/java/com/amir/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/amir/happyplaces/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.amir.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.amir.happyplaces.R
+import com.amir.happyplaces.database.DatabaseHandler
+import com.amir.happyplaces.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +19,19 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddHappyPlacesActivity::class.java)
             startActivity(intent)
         }
+        getHappyPlacesListFromLocalDB()
     }
+
+    private fun getHappyPlacesListFromLocalDB() {
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlaceList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+if(getHappyPlaceList.size >0){
+    for(i in getHappyPlaceList){
+        Log.e("title", i.title)
+        Log.e("Image",i.image)
+    }
+}
+    }
+
+
 }
