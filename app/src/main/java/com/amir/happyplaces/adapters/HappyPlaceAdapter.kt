c60e42a04fb0e6fece2b861da1999ff5c720b983
amir-android-dev <amir-hosseini@gmx.at>
2022-03-30 11:10:19 +0200

    14 RecyclerView Adapter and implement it in MainActivity.kt

---
+++ app/src/main/java/com/amir/happyplaces/adapters/HappyPlaceAdapter.kt
@@ -0,0 +1,44 @@
+package com.amir.happyplaces.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.amir.happyplaces.R
+import com.amir.happyplaces.models.HappyPlaceModel
+import kotlinx.android.synthetic.main.item_happy_place.view.*
+
+//this is an open class we can inherit
+open class HappyPlaceAdapter(
+    private val context: Context,
+    private var list: ArrayList<HappyPlaceModel>
+) :
+    RecyclerView.Adapter<HappyPlaceAdapter.MViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MViewHolder {
+        return MViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_happy_place, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MViewHolder, position: Int) {
+        val model = list[position]
+        if (holder is MViewHolder) {
+            holder.itemView.iv_place_image.setImageURI(Uri.parse(model.image))
+            holder.itemView.tvTitle.text = model.title
+            holder.itemView.tvDescription.text = model.description
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+
+    class MViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
---
+++ app/src/main/java/com/amir/happyplaces/models/HappyPlaceModel.kt
@@ -0,0 +1,13 @@
+package com.amir.happyplaces.models
+
+class HappyPlaceModel(
+    val id: Int,
+    val title:String,
+    val image: String,
+    val description:String,
+    val date: String,
+    val location: String,
+    val latitude:Double,
+    val longitude: Double
+) {
+}
